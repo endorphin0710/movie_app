fbe3c169f4ec639f9faf9b8e67c89e4caddfb093
juntae kim <endorphin0710@gmail.com>
2020-05-18 01:08:40 +0900

    Updates

---
+++ precache-manifest.53fbffcf5febdd1772e204d3be7cf948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389b060d06c6850c0db2349df0418d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab9a5fde135ad644184",
+    "url": "/static/css/main.b6c2da72.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab9a5fde135ad644184",
+    "url": "/static/js/main.37a2c7a7.chunk.js"
+  },
+  {
+    "revision": "0165e4c1b652714e4e64",
+    "url": "/static/js/runtime-main.e16a942a.js"
+  }
+]);
